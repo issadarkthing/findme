7135266ce28827fbd1ab8a8e9adef5f89d31adce
raziman <raziman.terra@gmail.com>
2024-02-29 17:27:36 +0800

    add device status

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,12 +3,13 @@ import { GPSState, getGps } from "@/action/getGps";
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import Skeleton from "./Skeleton";
-import Field from "./Field";
 const Map = dynamic(() => import("./Map"), { ssr: false });
 
 export default function Home() {
     const [gps, setGps] = useState<GPSState>();
-    const [deviceStatus, setDeviceStatus] = useState("offline");
+    const [deviceStatus, setDeviceStatus] = useState<"online" | "offline">(
+        "offline"
+    );
 
     useEffect(() => {
         const id = setInterval(async () => {
@@ -45,15 +46,59 @@ export default function Home() {
         <div>
             <Map lat={gps.lat} lon={gps.lon} date={gps.time} />
             <div className="border rounded-xl border-slate-800 mt-2 p-3 space-y-2">
-                <Field label="Status" value={deviceStatus} />
-                <Field label="Latitude" value={gps.lat} />
-                <Field label="Longitude" value={gps.lon} />
-                <Field label="Altitude" value={gps.alt} />
-                <Field
-                    label="Active Satellites"
-                    value={gps.satsActive?.length || 0}
-                />
+                <div>
+                    <p className="font-semibold">Device Status</p>
+                    <DeviceStatus status={deviceStatus} />
+                </div>
+                <div>
+                    <p className="font-semibold">Latitude</p> {gps.lat || "---"}
+                </div>
+                <div>
+                    <p className="font-semibold">Longitude</p>{" "}
+                    {gps.lon || "---"}
+                </div>
+                <div>
+                    <p className="font-semibold">Altitude</p> {gps.alt || "---"}
+                </div>
+                <div>
+                    <p className="font-semibold">Active Satellites</p>{" "}
+                    {gps.satsActive?.length || "---"}
+                </div>
             </div>
+        </div>
+    );
+}
+
+function DeviceStatus({
+    status,
+    gps,
+}: {
+    status: "offline" | "online";
+    gps?: GPSState;
+}) {
+    let gpsStatus = "gps detected";
+
+    if (!gps || !gps.satsActive) {
+        gpsStatus = "no satellite detected";
+    } else if (gps.satsActive.length < 3) {
+        gpsStatus = "weak satellite signal";
+    } else if (!gps.lat || !gps.lon) {
+        gpsStatus = "trying to detect location";
+    }
+
+    return (
+        <div className="flex gap-2 items-center">
+            <span className="relative flex h-3 w-3">
+                {status === "online" && (
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75" />
+                )}
+                <span
+                    className={`${
+                        status === "online" ? "bg-green-500" : "bg-gray-500"
+                    } relative inline-flex rounded-full h-3 w-3`}
+                />
+            </span>
+            {status || "---"} ({gpsStatus})
         </div>
     );
 }
