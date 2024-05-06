5d69831a6c02d88c6f5ecb54ecaef4c336a6924b
raziman <raziman.terra@gmail.com>
2024-05-06 15:17:49 +0800

    fix alignment

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -53,7 +53,7 @@ export default function Home() {
     return (
         <div className="flex flex-col items-center">
             <Map lat={gps.lat} lon={gps.lon} date={gps.lastUpdate} />
-            <div className="border rounded-xl border-slate-800 mt-2 p-3 space-y-2">
+            <div className="w-80 md:w-96 border rounded-xl border-slate-800 mt-2 p-3 space-y-2">
                 <div>
                     <p className="font-semibold">Device Status</p>
                     <DeviceStatus gps={gps} status={deviceStatus} />
